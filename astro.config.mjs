dd85f1d536951dcbb4a1805a729cfc0d9720025a
Micha macbook <ondergrondz@gmail.com>
2026-04-18 10:23:31 +0200

    add GitHub Actions workflow for deploying to GitHub Pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,6 +7,8 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://redzn.github.io',
+  base: '/port',
   integrations: [alpinejs()],
 
   vite: {
